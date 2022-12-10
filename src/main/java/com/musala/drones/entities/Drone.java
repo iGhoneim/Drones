efb1507c1241b73410141ab2ea8e8ab04b17b93b
Ghoneim <Ghoneim@outlook.com>
2022-12-10 23:47:06 +0200

    Validations

--- src/main/java/com/musala/drones/entities/Drone.java
+++ src/main/java/com/musala/drones/entities/Drone.java
@@ -1,6 +1,7 @@
 package com.musala.drones.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,16 +18,25 @@ import lombok.experimental.SuperBuilder;
 @Entity
 @Table
 public class Drone extends Base {
+    @Size(max = 100, message = "Drone's serial number cannot exceed 100 characters")
+    @NotBlank(message = "Drone's serial number is mandatory, and cannot be empty")
     @Column(name = Fields.serialNumber)
     private String serialNumber;
+    @NotNull(message = "Drone's model is mandatory, and cannot be empty")
     @Enumerated(EnumType.STRING)
     @Column(name = Fields.model)
     private Model model;
+    @DecimalMin(value = "0.0", message = "Drone's weight limit min. value is 0gr")
+    @DecimalMax(value = "500.0", message = "Drone's weight limit max. value is 500gr")
+    @NotNull(message = "Drone's weight limit is mandatory, and cannot be empty")
     @Column(name = Fields.weightLimit)
     private Float weightLimit;
+    @DecimalMin(value = "0.0", message = "Drone's battery capacity min. value is 0%")
+    @DecimalMax(value = "100.0", message = "Drone's battery capacity max. values is 100%")
     @Column(name = Fields.batteryCapacity)
-    private Float batteryCapacity;
+    private Float batteryCapacity = 100f;
+    @NotNull(message = "Drone's state is mandatory, and cannot be empty")
     @Enumerated(EnumType.STRING)
     @Column(name = Fields.state)
-    private State state;
+    private State state = State.IDLE;
 }
--- src/main/java/com/musala/drones/entities/Medication.java
+++ src/main/java/com/musala/drones/entities/Medication.java
@@ -3,6 +3,7 @@ package com.musala.drones.entities;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,10 +20,17 @@ import lombok.experimental.SuperBuilder;
 @Entity
 @Table
 public class Medication extends Base {
+    @Pattern(regexp = "^[\\w-]+$", message = "Medication's name allows only only letters, numbers, dashes, and underscores")
+    @NotBlank(message = "Medication's name is mandatory, and cannot be empty")
     @Column(name = Fields.name)
     private String name;
+    @DecimalMin(value = "0.0", message = "Medication's weight min. value is 0gr")
+    @DecimalMax(value = "500.0", message = "Medication's weight max. value is 500gr")
+    @NotNull(message = "Medication's weight is mandatory, and cannot be empty")
     @Column(name = Fields.weight)
     private Float weight;
+    @Pattern(regexp = "^[A-Z0-9_]+$", message = "Medication's code allows only only upper case letters, numbers, and underscores")
+    @NotBlank(message = "Medication's code is mandatory, and cannot be empty")
     @Column(name = Fields.code)
     private String code;
     @Column(name = Fields.image)
